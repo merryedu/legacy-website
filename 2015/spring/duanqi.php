86035bc8631ccc27926ce3e621002cadfa0f7279
merryedu <wxy@merryedu.com>
2015-04-17 15:04:47 +0800

    new duani form

---
+++ 2015/spring/duanqi.php
@@ -0,0 +1,12 @@
+<?php include( "partials/header.php" ); ?>
+<body>
+<!--wrapper starts-->
+    <div class="wrapper">
+        <?php 
+		$page = 'about';
+		?>
+        <?php include( "partials/menu.php" ); ?>
+    </div>
+<script src='https://www.jinshuju.net/f/jeOPjk/embedded.js?banner=show&background=white&height=1240'></script>
+
+<?php include( "partials/footer.php" ); ?>

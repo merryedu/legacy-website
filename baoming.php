5772f93a82c69d42751bcc0a3a6abcb1f1571162
merryedu <wxy@merryedu.com>
2015-05-13 11:44:14 +0800

    static file for baoming

--- baoming.php
+++ baoming.php
@@ -1,12 +1,5 @@
 <?php include( "partials/header.php" ); ?>
 <body>
-<!--wrapper starts-->
-    <div class="wrapper">
-        <?php 
-		$page = 'about';
-		?>
-        <?php include( "partials/menu.php" ); ?>
-    </div>
 <script src='https://www.jinshuju.net/f/UIhW0p/embedded.js?banner=show&background=white&height=1676'></script>
 
 <?php include( "partials/footer.php" ); ?>

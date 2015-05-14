52662c446ec64f52248d90fe480b8783cf743186
merryedu <wxy@merryedu.com>
2015-05-14 21:13:54 +0800

    upload teacher photos

--- partials/teachers_columns.php
+++ partials/teachers_columns.php
@@ -3,7 +3,7 @@
                         <div class="dt-sc-team">	
                             <div class="image">
                                 <img class="item-mask" src="images/mask.png" alt="" title="">
-                                <img src="http://placehold.it/381x331" alt="" title="">
+                                <img src="images/cm.png" alt="" title="丛明">
                                 <div class="dt-sc-image-overlay">
                                 	<a href="#" class="link"><span class="fa fa-link"></span></a>
                                     <a href="#" class="zoom"><span class="fa fa-search"></span></a>
@@ -11,7 +11,7 @@
                             </div>
                             <div class="team-details">
                                 <h4> 丛老师 </h4>
-                                <h6> 明睿名师 </h6>
+                                <h6> 明睿数学名师 </h6>
                                 <p> 毕业于南京大学，对数学有独特的理解，注重引导学生自我思考，提高自己解决问题的能力。在课堂上用简单的例子描述数学中深奥的道理，让学生不知不觉在思维上提高了一个层次，发现学习数学的美。<br>同时，丛老师在教学过程中不断的探索与创新，运用现代的教学手法向学生们讲解数学知识的精髓 </p>
                             </div>
                         </div>
@@ -20,16 +20,33 @@
                         <div class="dt-sc-team">	
                             <div class="image">
                                 <img class="item-mask" src="images/mask.png" alt="" title="">
-                                <img src="http://placehold.it/381x331" alt="" title="">
+                                <img src="images/syc.png" alt="" title="孙宇宸">
                                 <div class="dt-sc-image-overlay">
                                 	<a href="#" class="link"><span class="fa fa-link"></span></a>
                                     <a href="#" class="zoom"><span class="fa fa-search"></span></a>
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> X老师 </h4>
-                                <h6> 明睿名师 </h6>
-                                <p> 热爱数学，成绩优异。善于观察问题，分析问题，总结规律。秉持在理解中极易，在快乐中学习的教学理念，教会孩子数学知识并培养孩子数学思维，带领孩子领悟数学之美。上课细致耐心，善于与孩子交流，能与孩子交朋友，为孩子树立良好的学习榜样，引导孩子养成良好的学习习惯。 </p>
+                                <h4> 孙老师 </h4>
+                                <h6> 明睿数学名师 </h6>
+                                <p> 毕业于南京大学。高中时发表关于比赛中不定方程论文《不定方程: p^n+q^n=q^-(n-2)r^2》获得丘成桐数学竞赛南部赛区二等奖；2010年全国高中数学联赛一等奖；安徽省第四名进入省队；并保送南京大学。南京大学医学和经济学双学位。对数学有独到的见解，并常常用数学模型解决各种社会中得现象，善于用例子引导学生。坚持将抽象的概念形象化、具体化，使学生在实例中加深对概念的理解，并了解各种神奇高端的解决方法。兴趣广泛，热爱探索。 </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column dt-sc-one-fourth">
+                        <div class="dt-sc-team">	
+                            <div class="image">
+                                <img class="item-mask" src="images/mask.png" alt="" title="">
+                                <img src="images/csq.png" alt="" title="曹上青">
+                                <div class="dt-sc-image-overlay">
+                                	<a href="#" class="link"><span class="fa fa-link"></span></a>
+                                    <a href="#" class="zoom"><span class="fa fa-search"></span></a>
+                                </div>
+                            </div>
+                            <div class="team-details">
+                                <h4> 曹老师 </h4>
+                                <h6> 明睿数学名师、教研 </h6>
+                                <p> 毕业于南京大学数学系。小学通过三个小学数学竞赛一等奖保送到金陵中学初中部。在初中连续三年获得江苏省数学竞赛一等奖，加分到金陵中学高中部，在高中三年里认真专研数学竞赛，在高二，获得全国数学联赛一等奖，保送南大数学基础班。 从小就酷爱数学，对数学和数学教育充满热情。并且对待工作耐心、认真、负责，注重方法的举一反三，激发学生的学习兴趣，鼓励学生快乐学习，热情风趣，因材施教。注重用自己对待数学和生活的热情去感染学生，让学生爱数学，爱生活。 </p>
                             </div>
                         </div>
                     </div>
@@ -45,25 +62,8 @@
                             </div>
                             <div class="team-details">
                                 <h4> 马老师 </h4>
-                                <h6> 管理 </h6>
-                                <p> 学会关心，学会创造，全面发展，展露才华。 </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column dt-sc-one-fourth">
-                        <div class="dt-sc-team">	
-                            <div class="image">
-                                <img class="item-mask" src="images/mask.png" alt="" title="">
-                                <img src="http://placehold.it/381x331" alt="" title="">
-                                <div class="dt-sc-image-overlay">
-                                	<a href="#" class="link"><span class="fa fa-link"></span></a>
-                                    <a href="#" class="zoom"><span class="fa fa-search"></span></a>
-                                </div>
-                            </div>
-                            <div class="team-details">
-                                <h4> 王老师 </h4>
-                                <h6> 教研 </h6>
-                                <p> 数学的学习不是死记硬背，而是要靠理解和体悟。良好的数学学习习惯可以使数学学习变得更加轻松容易。 </p>
+                                <h6> 咨询、管理 </h6>
+                                <p> 学会关心，学会创造，全面发展，展露才华。数学的学习不是死记硬背，而是要靠理解和体悟。良好的数学学习习惯可以使数学学习变得更加轻松容易。 </p>
                             </div>
                         </div>
                     </div>

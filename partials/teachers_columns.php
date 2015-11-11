4d8647837d75c8112d4ea9fdb3e9865afa36d76d
merryedu <wxy@merryedu.com>
2015-11-11 11:32:27 +0800

    update teacher info

--- partials/teachers_columns.php
+++ partials/teachers_columns.php
@@ -10,9 +10,26 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 丛老师 </h4>
+                                <h4> 丛明老师 </h4>
                                 <h6> 明睿数学名师 </h6>
                                 <p> 毕业于南京大学，对数学有独特的理解，注重引导学生自我思考，提高自己解决问题的能力。在课堂上用简单的例子描述数学中深奥的道理，让学生不知不觉在思维上提高了一个层次，发现学习数学的美。<br>同时，丛老师在教学过程中不断的探索与创新，运用现代的教学手法向学生们讲解数学知识的精髓 </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column dt-sc-one-fourth">
+                        <div class="dt-sc-team">
+                            <div class="image">
+                                <img class="item-mask" src="images/mask.png" alt="" title="">
+                                <img src="images/wqj.jpg" alt="" title="王奇君">
+                                <div class="dt-sc-image-overlay">
+                                	<a href="#" class="link"><span class="fa fa-link"></span></a>
+                                    <a href="#" class="zoom"><span class="fa fa-search"></span></a>
+                                </div>
+                            </div>
+                            <div class="team-details">
+                                <h4> 王奇君老师 </h4>
+                                <h6> 明睿数学名师 </h6>
+                                <p> 上海外国语大学，传播学硕士学历。曾为新东方名师，有多年英语数学的教学经验，谙熟国外初高中数学教学，经过她的A-level和雅思 考试辅导后成功出国深造的学生不计其数。她数学功底和逻辑思维能力极强，在最讲究缜密的逻辑能力的辩论比赛中体现得淋漓尽致，曾获全国外研社杯英语辩论大 赛一等奖、复旦大学大师杯辩论赛冠军，首尔国立大学全韩辩论赛4强。她本人的英语实力也是不容小觑，持高级口译证书，曾多次作为翻译或同传出席政府会议。 她能让喜欢上数学和逻辑的同学们的头脑和语言能力都锦上添花。<br>在教学风格上，王老师耐心严格、风趣幽默，“什么都是有方法的”成为她最有感染力的学习精神。</p>
                             </div>
                         </div>
                     </div>
@@ -27,7 +44,7 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 孙老师 </h4>
+                                <h4> 孙宇宸老师 </h4>
                                 <h6> 明睿数学名师 </h6>
                                 <p> 毕业于南京大学。高中时发表关于比赛中不定方程论文《不定方程: p^n+q^n=q^-(n-2)r^2》获得丘成桐数学竞赛南部赛区二等奖；2010年全国高中数学联赛一等奖；安徽省第四名进入省队；并保送南京大学。南京大学医学和经济学双学位。对数学有独到的见解，并常常用数学模型解决各种社会中得现象，善于用例子引导学生。坚持将抽象的概念形象化、具体化，使学生在实例中加深对概念的理解，并了解各种神奇高端的解决方法。兴趣广泛，热爱探索。 </p>
                             </div>
@@ -44,13 +61,14 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 曹老师 </h4>
+                                <h4> 曹上青老师 </h4>
                                 <h6> 明睿数学名师、教研 </h6>
                                 <p> 毕业于南京大学数学系。小学通过三个小学数学竞赛一等奖保送到金陵中学初中部。在初中连续三年获得江苏省数学竞赛一等奖，加分到金陵中学高中部，在高中三年里认真专研数学竞赛，在高二，获得全国数学联赛一等奖，保送南大数学基础班。 从小就酷爱数学，对数学和数学教育充满热情。并且对待工作耐心、认真、负责，注重方法的举一反三，激发学生的学习兴趣，鼓励学生快乐学习，热情风趣，因材施教。注重用自己对待数学和生活的热情去感染学生，让学生爱数学，爱生活。 </p>
                             </div>
                         </div>
                     </div>
-                    <div class="column dt-sc-one-fourth">
+                    <div class="dt-sc-hr"></div>
+                    <div class="column dt-sc-one-fourth first">
                         <div class="dt-sc-team">
                             <div class="image">
                                 <img class="item-mask" src="images/mask.png" alt="" title="">
@@ -61,14 +79,13 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 张老师 </h4>
+                                <h4> 张敏勤老师 </h4>
                                 <h6> 明睿数学名师、教研 </h6>
                                 <p> 来自东南大学热力专业的理科系软妹纸一枚，人萌课萌，能用萌萌搭，温柔柔的方式给孩子教授数学。喜欢孩子，擅长与孩子互动，带着南方姑娘的亲切和善，上课时娓娓道来，坚持授之以鱼不如授之以渔，用着自己独特的方式引领着“萌数学”的风格。 </p>
                             </div>
                         </div>
                     </div>
-                    <div class="dt-sc-hr"></div>
-                    <div class="column dt-sc-one-fourth first">
+                    <div class="column dt-sc-one-fourth">
                         <div class="dt-sc-team">
                             <div class="image">
                                 <img class="item-mask" src="images/mask.png" alt="" title="">
@@ -79,7 +96,7 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 高老师 </h4>
+                                <h4> 高爽老师 </h4>
                                 <h6> 明睿幼升小，低年级数学名师 </h6>
                                 <p> 来自南京师范大学，带着东北妹子的爽朗性格与良好口才，最擅长的事情就是与孩子相处，能带来活跃、快乐的课堂气氛。能随时随地化身“小朋友”，用孩子最喜欢、最能理解的方式讲述数学知识，是最可爱最能嗨翻课堂的女老师，是爱笑爱自拍爱和孩子交流的女汉子。 </p>
                             </div>
@@ -96,7 +113,7 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 陶老师 </h4>
+                                <h4> 陶雨薇老师 </h4>
                                 <h6> 明睿高年级数学名师 </h6>
                                 <p> 来自南京大学，是淮左名都扬州的一名净身高一米七的“高”冷女生。不仅有着居高临下的身高，还有着居高临下的数学成绩，在“恐怖”的江苏数学小升初、中考、高考中（143+34）均考出接近满分的傲人成绩。解题时一秒高冷，思路清晰方法严谨；讲题时深入浅出，能用简洁明了的方式让孩子理解数学道理，培养良好习惯。调皮的孩子，敢不敢来感受一下“高冷”陶老师的风格？ </p>
                             </div>
@@ -113,13 +130,14 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 席老师 </h4>
+                                <h4> 席志远老师 </h4>
                                 <h6> 明睿高年级数学名师 </h6>
                                 <p> 来自南京大学理工院系、有着三年数学教学经验的温柔男生，呆萌、幽默、犀利、清晰多种矛盾因素集于一身，既能化身冷静理科男给孩子讲述数学道理，又能变成欢脱大男孩与孩子一起探讨数学问题，帮助过许多孩子在数学上获得巨大进步，想对多重风格了解更多，请紧盯席老师回眸一笑时真诚的眼睛。 </p>
                             </div>
                         </div>
                     </div>
-                    <div class="column dt-sc-one-fourth">
+                    <div class="dt-sc-hr"></div>
+                    <div class="column dt-sc-one-fourth first">
                         <div class="dt-sc-team">
                             <div class="image">
                                 <img class="item-mask" src="images/mask.png" alt="" title="">
@@ -130,13 +148,12 @@
                                 </div>
                             </div>
                             <div class="team-details">
-                                <h4> 王彤彤 </h4>
+                                <h4> 王彤彤老师 </h4>
                                 <h6> 明睿教研小组成员 </h6>
                                 <p> 来自南京大学数学系基础数学专业，热爱数学并且乐于将知识传授给学生，善于总结数学解题中的方法和规律，善于与学生进行交流，充分发掘学生的潜能，使学生从被动接受的“要我学”转化为主动的“我要学”，变“学会”为“会学”，让学生学数学是一件快乐的事，并且工作认真、负责。曾多次参加江苏省高考阅卷与奥数阅卷工作，对应试、得分技巧比较了解。</p>
                             </div>
                         </div>
                     </div>
-                    <div class="dt-sc-hr"></div>
                     <div class="column dt-sc-one-fourth">
                         <div class="dt-sc-team">
                             <div class="image">

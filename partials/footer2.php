088023504aa4869abb3e8ce9f99bf2b4d8758512
merryedu <wxy@merryedu.com>
2015-11-12 15:09:38 +0800

    update location

--- partials/footer2.php
+++ partials/footer2.php
@@ -68,7 +68,8 @@
                         <aside class="widget widget_text">
                         <h3 class="widgettitle steelblue_sketch">联系方式</h3>
                             <div class="textwidget">
-                                <p class="dt-sc-contact-info"><span class="fa fa-map-marker"></span> 南京仙林大学城 <br> 东方天郡 </p>
+                                <p class="dt-sc-contact-info"><span class="fa fa-map-marker"></span> 南京仙林大学城杉湖西路8号东方天郡西区23栋1单元103</p>
+                                <p class="dt-sc-contact-info"><span class="fa fa-map-marker"></span> 中山东路198号龙台国际大厦12楼1215室</p>
                                 <p class="dt-sc-contact-info"><span class="fa fa-phone"></span> (+86) 131-828-12306 </p>
                                 <p class="dt-sc-contact-info"><span class="fa fa-envelope"></span><a href="mailto:wxy@merryedu.com"> wxy@merryedu.com </a></p>
                                 <p class="dt-sc-contact-info"><span class="fa fa-qq"></span> QQ号: 2848540164 </a></p>

08a112052b8ddbaf77dd2313a80fdd0772f970b5
merryedu <wxy@merryedu.com>
2015-11-21 20:48:28 +0800

    update refer

---
+++ refer.php
@@ -0,0 +1,27 @@
+<?php include( "partials/header.php" ); ?>
+<body>
+	<!--wrapper starts-->
+    <div class="wrapper">
+        <?php 
+		$page = 'baoming';
+		?>
+        <?php include( "partials/menu.php" ); ?>
+        <!--main starts-->
+        <div id="main">
+        	<!--breadcrumb-section starts-->
+            <div class="breadcrumb-section">
+            	<div class="container">
+                	<h1>新生报名</h1>
+                    <div class="breadcrumb">
+                        <a href="index.php">主页</a>
+                        <span class="fa fa-angle-double-right"></span>
+                        <span class="current">新生报名</span>
+                    </div>
+                </div>
+            </div>
+            <!--breadcrumb-section ends-->
+         <script src='https://jinshuju.net/f/7pasIU/embedded.js?banner=show&background=white&height=749'></script>
+		</div>
+	</div>
+
+<?php include( "partials/footer.php" ); ?>

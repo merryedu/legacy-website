1badd64478dd570aa3b428234cd6ea33f3429c87
merryedu <wxy@merryedu.com>
2016-04-14 17:08:56 +0800

    fix baoming

--- baoming.php
+++ baoming.php
@@ -2,7 +2,7 @@
 <body>
 	<!--wrapper starts-->
     <div class="wrapper">
-        <?php 
+        <?php
 		$page = 'baoming';
 		?>
         <?php include( "partials/menu.php" ); ?>
@@ -20,7 +20,7 @@
                 </div>
             </div>
             <!--breadcrumb-section ends-->
-			<script src='https://www.jinshuju.net/f/UIhW0p/embedded.js?banner=show&background=white&height=1676'></script>
+			<script src="https://jinshuju.net/f/UIhW0p/embedded.js?banner=show&background=white&height=1382"></script>
 		</div>
 	</div>
 

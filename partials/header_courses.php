eb2c6eb921c61f11445f7f61788f785425c9f2aa
merryedu <wxy@merryedu.com>
2016-04-16 23:24:38 +0800

    update header

---
+++ partials/header_courses.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-gb" class="no-js"> <!--<![endif]-->
+<head>
+<meta http-equiv="content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>明睿数学课程</title>
+<meta name="description" content="明睿教育是南京一家专注于小学K12培优的教育机构">
+<meta name="author" content="merryedu">
+<!-- Fav and touch icons -->
+<link rel="shortcut icon" href="images/ico/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="57x57" href="images/ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/ico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/ico/favicon-16x16.png">
+<link rel="manifest" href="images/ico/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="images/ico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<link href="style.css" rel="stylesheet" type="text/css">
+<link href="css/shortcodes.css" rel="stylesheet" type="text/css">
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/shortcodes.css" rel="stylesheet" type="text/css">
+<!--prettyPhoto-->
+<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css" media="all" />
+<!--[if IE 7]>
+<link href="css/font-awesome-ie7.css" rel="stylesheet" type="text/css">
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+<!--Fonts-->
+<!--<link href='http://fonts.useso.com/css?family=Lato:300,400,700,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.useso.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.useso.com/css?family=Bubblegum+Sans' rel='stylesheet' type='text/css'>-->
+<!--jquery-->
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+</head>

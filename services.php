7effa24db25ac8ccd145e76c04311a9db15178b1
merryedu <wxy@merryedu.com>
2016-10-29 12:06:57 +0800

    add 2016-2017 winter spring courses info

--- services.php
+++ services.php
@@ -106,6 +106,16 @@
                     </ul>
                 </div>
 
+                <div class="column dt-sc-one-third">
+                    <ul class="dt-sc-fancy-list pink smile">
+                        <li><a href="/doc/2016_2017_grade_1_winter_spring_courses.htm?update=1">2016-2017寒春一年级课程安排</li>
+                        <li><a href="/doc/2016_2017_grade_2_winter_spring_courses.htm?update=1">2016-2017寒春二年级课程安排</a></li>
+                        <li><a href="/doc/2016_2017_grade_3_winter_spring_courses.htm?update=1">2016-2017寒春三年级课程安排</a></li>
+                        <li><a href="/doc/2016_2017_grade_4_winter_spring_courses.htm?update=1">2016-2017寒春四年级课程安排</a></li>
+                        <li><a href="/doc/2016_2017_grade_5_winter_spring_courses.htm?update=1">2016-2017寒春五年级课程安排</a></li>
+                    </ul>
+                </div>
+
                 </div>
                 <!--container ends-->
                 <div class="dt-sc-hr"></div>

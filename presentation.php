8cc15f8ddaff742fdc46d0a67431122be1756f04
merryedu <wxy@merryedu.com>
2016-11-10 14:48:54 +0800

    upload ppt

---
+++ presentation.php
@@ -0,0 +1,49 @@
+<?php include( "partials/header.php" ); ?>
+<body>
+	<!--wrapper starts-->
+    <div class="wrapper">
+        	    <?php
+		$page = 'materials';
+		?>
+        <?php include( "partials/menu.php" ); ?>
+        <!--main starts-->
+        <div id="main">
+        	<!--breadcrumb-section starts-->
+            <div class="breadcrumb-section">
+            	<div class="container">
+                	<h1>家长会PPT</h1>
+                    <div class="breadcrumb">
+                        <a href="index.php">主页</a>
+                        <span class="fa fa-angle-double-right"></span>
+                        <span class="current">家长会PPT</span>
+                    </div>
+                </div>
+            </div>
+            <!--breadcrumb-section ends-->
+            <!--container starts-->
+            <div class="container">
+            	<!--primary starts-->
+            	<section id="primary" class="content-full-width">
+
+                	<h2 class="dt-sc-hr-green-title">下载家长会PPT</h2>
+                    <div class="column dt-sc-one-half first">
+	                      <a href="/download/2016至2017寒春一年级家长会.pdf" class="dt-sc-button small"> <span class="fa fa-file-pdf-o"> 2016至2017寒春一年级家长会PPT <span class="fa fa-download"> </span></a>
+                        <div class="dt-sc-clear"></div>
+                        <a href="/download/2016至2017寒春二年级家长会.pdf" class="dt-sc-button small"> <span class="fa fa-file-pdf-o"> 2016至2017寒春二年级家长会PPT <span class="fa fa-download"> </span></a>
+                        <div class="dt-sc-clear"></div>
+                        <a href="/download/2016至2017寒春三年级家长会.pdf" class="dt-sc-button small"> <span class="fa fa-file-pdf-o"> 2016至2017寒春三年级家长会PPT <span class="fa fa-download"> </span></a>
+                        <div class="dt-sc-clear"></div>
+                        <a href="/download/2016至2017寒春四年级家长会.pdf" class="dt-sc-button small"> <span class="fa fa-file-pdf-o"> 2016至2017寒春四年级家长会PPT <span class="fa fa-download"> </span></a>
+                        <div class="dt-sc-clear"></div>
+                        <a href="/download/2016至2017寒春五年级家长会.pdf" class="dt-sc-button small"> <span class="fa fa-file-pdf-o"> 2016至2017寒春五年级家长会PPT <span class="fa fa-download"> </span></a>
+                    <div class="dt-sc-hr"></div>
+
+                </section>
+                <!--primary ends-->
+            </div>
+            <!--container ends-->
+        </div>
+        <!--main ends-->
+
+        <!--footer starts-->
+<?php include( "partials/footer.php" ); ?>

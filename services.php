9e30b3fb98d8704412343b97ab0e387ae27ca488
merryedu <wxy@merryedu.com>
2019-04-28 21:55:18 +0800

    update courses

--- services.php
+++ services.php
@@ -85,19 +85,7 @@
                 <div class="dt-sc-hr"></div>
 
                 <div class="container">
-                <h2 class="dt-sc-hr-green-title">2018年明睿数学课程</h2>
-
-                <div class="column dt-sc-one-third">
-                    <ul class="dt-sc-fancy-list mustard star">
-                        <li><a href="/doc/2018_new_grade_1_summer_autumn_courses.htm?update=101">2018暑秋新一年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_2_summer_autumn_courses.htm?update=101">2018暑秋新二年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_3_summer_autumn_courses.htm?update=101">2018暑秋新三年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_4_summer_autumn_courses.htm?update=101">2018暑秋新四年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_5_summer_autumn_courses.htm?update=101">2018暑秋新五年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_6_summer_autumn_courses.htm?update=101">2018暑秋新六年级课程安排</a></li>
-                        <li><a href="/doc/2018_new_grade_7_summer_autumn_courses.htm?update=101">2018暑秋新初一课程安排</a></li>
-                    </ul>
-                </div>
+                <h2 class="dt-sc-hr-green-title">2019年明睿数学课程</h2>
 
                 <div class="column dt-sc-one-third">
                     <ul class="dt-sc-fancy-list pink smile">
@@ -108,6 +96,19 @@
                         <li><a href="/doc/2018_grade_5_winter_spring_courses.htm?update=2">2018-2019寒春五年级课程安排</a></li>
                         <li><a href="/doc/2018_grade_6_winter_spring_courses.htm?update=2">2018-2019寒春六年级课程安排</a></li>
                         <li><a href="/doc/2018_grade_7_winter_spring_courses.htm?update=2">2018-2019寒春初一年级课程安排</a></li>
+                    </ul>
+                </div>
+
+                <div class="column dt-sc-one-third">
+                    <ul class="dt-sc-fancy-list mustard star">
+                        <li><a href="/doc/2019_new_grade_1_summer_autumn_courses.htm?update=1">2019暑秋新一年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_2_summer_autumn_courses.htm?update=1">2019暑秋新二年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_3_summer_autumn_courses.htm?update=1">2019暑秋新三年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_4_summer_autumn_courses.htm?update=1">2019暑秋新四年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_5_summer_autumn_courses.htm?update=1">2019暑秋新五年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_6_summer_autumn_courses.htm?update=1">2019暑秋新六年级课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_7_summer_autumn_courses.htm?update=1">2019暑秋新初一课程安排</a></li>
+                        <li><a href="/doc/2019_new_grade_8_summer_autumn_courses.htm?update=1">2019暑秋新初二课程安排</a></li>
                     </ul>
                 </div>
 
